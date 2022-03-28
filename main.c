8b8670dc9c6d7d91786dcd3a2d27fb87b97406db
Uhorkovy privarok <102039070+uhorkovyprivarok@users.noreply.github.com>
2022-03-28 11:09:01 +0200

    Update main.c
    Changes:
    
        main.c :
        - cleaned-up, added empty infinite while loop
        mcu.c, function mcu_initSpia :
        - removed FIFO settings, FIFO was not compatible with the library as library always waits for transmission to complete
        - added GPIO14 configuration as general output to control SPI_PD
        - added condition to ensure, that BRR will not overflow, so min.
        baudrate is 295 kHz
        EVE_target.h :
        - corrected spi_transmit(uint8_t data), added a return value - see comments in file

--- main.c
+++ main.c
@@ -13,9 +13,6 @@
 
 
 
-// blblbllbla
-
-// OPYTAJ SA:   kolko je DELAY_US(1)
 
 
 #include "EVE_commands.h"
@@ -45,57 +42,19 @@ int main(void)
     // init gpio
     gpio_init();
 
-    // initialize SPI-A to 100 kBaud/s = 100 kHz
-   // mcu_initSpia(100000);
-    mcu_initSpia(500000); // 500kHz
+    mcu_initSpia(500000); // set to 500 kHz
 
     //intr_initInterrupts();
 
-    //CLK_PHASE = 1;
-    //CLKPOLARITY = 0;
 
-    EVE_init();
-
-/*
-    EVE_cmd_dl(CMD_DLSTART); // tells EVE to start a new display-list
-    EVE_cmd_dl(DL_CLEAR_RGB | 0xFFFFFF); // sets the background color
-    EVE_cmd_dl(DL_CLEAR | CLR_COL | CLR_STN | CLR_TAG);
-    EVE_color_rgb(0x0);
-    EVE_cmd_text(5, 15, 28, 0, "Hello there!");
-    EVE_cmd_dl(DL_DISPLAY); // put in the display list to mark its end
-    EVE_cmd_dl(CMD_SWAP); // tell EVE to use the new display list
-    while (EVE_busy());
-*/
+   EVE_init();
 
 
-/*    int y = 0;
-    int x = (0 & 0xFF) << 8;;
-    char gayy[] = {"MCU_SPI_CLK_Freq(<11MHz);"};
-
-    //spi_sendChar("C");
-    while(1)
+   while(1)
     {
-        spi_sendData(gayy, 20);
-    }
-*/
-
-
-/*    while(1)
-    {
-        if (PB2_PRESSED)
-            {
-            LD3_ON;
-            LD2_ON;
-            }
-        else
-            {
-            LD2_OFF;
-            LD3_OFF;
-            }
-
 
     }
-*/
+
 
 
 	return 0;
